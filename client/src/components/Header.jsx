1f7205f6dff38d6ca1d21c557c9d09dd35009823
sarang joshi <sarangjoshi196@gmail.com>
2024-03-29 18:08:28 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
                 </Button>
 
                 <Link to='/sign-in'>
-                    <Button gradientDuoTone='purpleToBlue' >
+                    <Button gradientDuoTone='purpleToBlue' outline >
                         Sign In
                     </Button>
                 </Link>
