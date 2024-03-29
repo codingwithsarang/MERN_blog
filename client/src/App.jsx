4aa6e0e8d4c820bdb2d2be1d2ff449030b5b71dc
sarang joshi <sarangjoshi196@gmail.com>
2024-03-29 17:58:03 +0530

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import { Home,Signin,Signup, Dashboard, About,Projects } from './pages'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <BrowserRouter>
+    <Header /> 
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+import About from './About'
+import Signin from './Sign-In'
+import Signup from './Sign-Up'
+import Dashboard from './Dashboard'
+import Projects from './Projects'
+import Home from './Home'
+
+
+export {About, Signin, Signup, Dashboard, Projects,Home}
