adc460a981a6d584e9b85758add2ccd039d6c6a1
sarang joshi <sarangjoshi196@gmail.com>
2024-03-31 15:31:17 +0530

    create sign in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req,res, next)=>{
     let {email, username, password} = req.body
@@ -22,4 +23,41 @@ export const signup = async (req,res, next)=>{
     } catch (error) {
         next(error)
     }
+}
+
+
+export const signin = async(req,res, next)=>{
+        const {email, password} = req.body
+
+        if(!email || !password || email === '' || password === ''){
+           return next(errorHandler(400, 'All fiields are required'))
+        }
+
+
+        try{
+            const validUser = await User.findOne({email})
+
+            if(!validUser){
+                return next(errorHandler(404, 'User not Found'))
+            }
+
+            const validPassword = bcryptjs.compareSync(password, validUser.password )
+            if(!validPassword){
+                return next(errorHandler(400, 'Invalid Password'))
+            }
+            
+            const {password: pass, ...rest} = validUser._doc
+
+            const token = jwt.sign(
+                {id: validUser._id} , process.env.JWT_SECRET
+            )
+            
+
+            res.status(200).cookie('access_token', token, {
+                httpOnly: true
+            }).json(rest)
+                
+        } catch(error){
+            next(error)
+        }
 }
